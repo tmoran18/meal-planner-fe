f32e2110ad11617da3ecc86862c082e55bdffcd5
tmoran18 <moran.tim01@gmail.com>
2021-02-20 15:48:21 +1000

    Working on Meal component

--- components/Meals.js/Meals.js
+++ components/Meals.js/Meals.js
@@ -1,21 +1,23 @@
-import { useContext } from 'react';
-import MealContext from '../../context/meal/mealContext';
+import { useContext } from "react";
+import MealContext from "../../context/meal/mealContext";
+import styles from "../Meals.js/meals.module.css";
 
 const Meals = () => {
-	const mealContext = useContext(MealContext);
+  const mealContext = useContext(MealContext);
 
-	const { meals } = mealContext;
+  const { meals } = mealContext;
 
-	return (
-		<>
-			{meals.map((meal) => (
-				<div>
-					<h3>{meal.name}</h3>
-					<img src={meal.image_URL} width='200' alt='' />
-				</div>
-			))}
-		</>
-	);
+  return (
+    <>
+      {meals.map((meal) => (
+        <div className={styles.meal__card}>
+          <img src={meal.image_URL} width="350" alt="" />
+          <h3 className={styles.meal__title}>{meal.name}</h3>
+          <p className={styles.meal__sec_title}>{meal.secondary_name}</p>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Meals;
